82ed46d1cf6a64abaa013c051e08c3597c6bd094
jmshanahan <comeraghsolutions@gmail.com>
2018-02-12 20:50:32 +0000

    Login and signup working but not complete

--- server.js
+++ server.js
@@ -11,33 +11,33 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 
 const configDB = require('./config/database');
-
+mongoose.Promise = global.Promise;
 // configuration section
-mongoose.connect(configDB.url,(err, db) => {
-  if(err){
+mongoose.connect(configDB.url, (err, db) => {
+  if (err) {
       return console.log('Unable to connect to the MongoDB server');
   }
   console.log('Connected to MongoDB server');
 });
+require('./config/passport')(passport);
+
 // Setup express application
-app.use(morgan('dev')); //log every request out to the console
+app.use(morgan('dev')); // log every request out to the console
 app.use(cookieParser()); // read cookies. Needed for auth
-app.use(bodyParser.urlencoded({extended:true}));   // get information from html forms
+app.use(bodyParser.urlencoded({extended: true})); // get information from html forms
 
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs'); //setup the ejs for templating
+app.set('view engine', 'ejs'); // setup the ejs for templating
 
 // Config password model
 app.use(session({secret: 'tbWDUe53uBegqRGD2SD3dae1Z8aoKq3k'}));
 app.use(passport.initialize());
-app.use(passport.session()); //persistent login sessions
+app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect flash for flash messages stored in session
 // routes
-require('./app/routes')(app,passport);
+require('./app/routes')(app, passport);
 
-app.listen(port,() => {
+app.listen(port, () => {
   console.log(`The magic happens on ${port}`);
 });
-
-
 
